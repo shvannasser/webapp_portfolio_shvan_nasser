0c44a32188d2f39d0f27b2393d6b85d022dc6be4
GrapeBird <shvann@hiof.no>
2024-09-26 17:50:01 +0200

    create project form optional , close : open

--- portfolio2/frontend/src/components/AddProjectForm.tsx
+++ portfolio2/frontend/src/components/AddProjectForm.tsx
@@ -18,6 +18,12 @@ export default function AddProjectForm(props: AddProjectFormProps) {
     description: "",
   });
 
+  const [show, setShow] = useState(false);
+
+  const showProjects = () => {
+    setShow(!show);
+  };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (!project.title || !project.description)
@@ -27,36 +33,46 @@ export default function AddProjectForm(props: AddProjectFormProps) {
 
   return (
     <section className='formContainer'>
-      <form onSubmit={handleSubmit} className='projectForm'>
-        <label htmlFor='title'>Title</label>
-        <input
-          type='text'
-          placeholder='Enter project title'
-          id='title'
-          value={project.title}
-          // onChange = {handleChange}
-          onChange={(e) => setProject({ ...project, title: e.target.value })}
-        />
-        <label htmlFor='image'>Image</label>
-        <input
-          type='text'
-          placeholder='Enter project image'
-          id='image'
-          value={project.image}
-          onChange={(e) => setProject({ ...project, image: e.target.value })}
-        />
-        <label htmlFor='description'>Description</label>
-        <input
-          type='text'
-          placeholder='Enter project description'
-          id='description'
-          value={project.description}
-          onChange={(e) =>
-            setProject({ ...project, description: e.target.value })
-          }
-        />
-        <button type='submit'>Add Project</button>
-      </form>
+      <button
+        onClick={showProjects}
+        // Gjør at vi kan velge mellom to forskjellige klasser basert på state til show mtp CSS.
+        // forskjell mellom ? og ?? er at ?? sjekker om det er null eller undefined mens ? sjekker om det er true eller false.
+        className={show ? "close-form-button" : "show-form-button"}
+      >
+        {show ? "Close form" : "Create a new project"}
+      </button>{" "}
+      {show && (
+        <form onSubmit={handleSubmit} className='projectForm'>
+          <label htmlFor='title'>Title</label>
+          <input
+            type='text'
+            placeholder='Enter project title'
+            id='title'
+            value={project.title}
+            // onChange = {handleChange}
+            onChange={(e) => setProject({ ...project, title: e.target.value })}
+          />
+          <label htmlFor='image'>Image</label>
+          <input
+            type='text'
+            placeholder='Enter project image'
+            id='image'
+            value={project.image}
+            onChange={(e) => setProject({ ...project, image: e.target.value })}
+          />
+          <label htmlFor='description'>Description</label>
+          <input
+            type='text'
+            placeholder='Enter project description'
+            id='description'
+            value={project.description}
+            onChange={(e) =>
+              setProject({ ...project, description: e.target.value })
+            }
+          />
+          <button type='submit'>Add Project</button>
+        </form>
+      )}
     </section>
   );
 }
